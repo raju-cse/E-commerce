a1a74802065432fde27914ade9c03956fa43aa1d
Your Name <your.email@example.com>
2025-10-09 11:08:59 +0600

    Database Connection Setup

---
+++ infra/db/connection.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func GetConnectionString() string{
+	// user - > postgres
+	// password -> password
+	// host -> localhost
+	// port - > 5432
+	// db name -> ecommerce
+
+	return "user=postgres password=password host=localhost port=5432 dbname=ecommerce"
+}
+
+func NewConnection() (*sqlx.DB, error){
+	dbSource := GetConnectionString()
+	dbCon, err := sqlx.Connect("postgres", dbSource)
+
+	if err != nil{
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return dbCon, nil
+}
